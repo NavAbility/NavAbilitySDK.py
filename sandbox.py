cfe4271043f7c79633903719e61b9c4637ef92d3
Sam Claassens <sam@globus.org>
2021-12-21 00:22:34 -0600

    Fixing linting

--- sandbox.py
+++ sandbox.py
@@ -1,61 +1,77 @@
+# flake8: noqa
+
 import time
-from navability.entities.Variable import Variable
-from navability.entities.Factor import Factor, FactorPrior
-from navability.entities.StatusMessage import StatusMessage
-from navability.services.NavAbilityClient import NavAbilityClient
 from pprint import pprint
 
-navi = NavAbilityClient()
-result = navi.addVariable(Variable("x0", "Pose2"))
-time.sleep(1)
-statuses = navi.getStatusMessages(result['addVariable'])
-pprint(statuses)
-result = navi.addVariable(Variable("x1", "Pose2"))
-time.sleep(1)
-statuses = navi.getStatusMessages(result['addVariable'])
-pprint(statuses)
+from navability.entities.Client import Client
+from navability.entities.Factor import Factor, FactorPrior
+from navability.entities.NavAbilityClient import NavAbilityWebsocketClient
+from navability.entities.StatusMessage import StatusMessage
+from navability.entities.Variable import Variable
+from navability.services.Factor import addFactor
+from navability.services.Solve import solveSession
+from navability.services.Status import getStatusMessages
+from navability.services.Variable import addVariable
 
-# Add a prior
-result = navi.addFactor(FactorPrior("x0f1", "PriorPose2", ["x0"]))
-time.sleep(1)
-statuses = navi.getStatusMessages(result['addFactor'])
-pprint(statuses)
+client = Client("Guest", "DemoRobot", "DemoSession")
+navAbilityClient = NavAbilityWebsocketClient()
+v = Variable("x0", "Pose2")
+res = addVariable(navAbilityClient, client, v)
+getStatusMessages(navAbilityClient, res["addVariable"])
+solveSession(navAbilityClient, client)
 
-# Add a factor
-result = navi.addFactor(Factor("x0x1f1", "Pose2Pose2", ["x0", "x1"]))
-time.sleep(1)
-statuses = navi.getStatusMessages(result['addFactor'])
-pprint(statuses)
+# navi = NavAbilityClient()
+# result = navi.addVariable(Variable("x0", "Pose2"))
+# time.sleep(1)
+# statuses = navi.getStatusMessages(result['addVariable'])
+# pprint(statuses)
+# result = navi.addVariable(Variable("x1", "Pose2"))
+# time.sleep(1)
+# statuses = navi.getStatusMessages(result['addVariable'])
+# pprint(statuses)
 
-# WIP
-result = navi.solveSession()
+# # Add a prior
+# result = navi.addFactor(FactorPrior("x0f1", "PriorPose2", ["x0"]))
+# time.sleep(1)
+# statuses = navi.getStatusMessages(result['addFactor'])
+# pprint(statuses)
+
+# # Add a factor
+# result = navi.addFactor(Factor("x0x1f1", "Pose2Pose2", ["x0", "x1"]))
+# time.sleep(1)
+# statuses = navi.getStatusMessages(result['addFactor'])
+# pprint(statuses)
+
+# # WIP
+# result = navi.solveSession()
 
 
-#### Sandbox
+# #### Sandbox
 
-from functools import wraps
-from time import time
-def timing(f):
-    @wraps(f)
-    def wrap(*args, **kw):
-        ts = time()
-        result = f(*args, **kw)
-        te = time()
-        print 'func:%r args:[%r, %r] took: %2.4f sec' % \
-          (f.__name__, args, kw, te-ts)
-        return result
-    return wrap
+# from functools import wraps
+# from time import time
+# def timing(f):
+#     @wraps(f)
+#     def wrap(*args, **kw):
+#         ts = time()
+#         result = f(*args, **kw)
+#         te = time()
+#         print 'func:%r args:[%r, %r] took: %2.4f sec' % \
+#           (f.__name__, args, kw, te-ts)
+#         return result
+#     return wrap
 
 
 ## Push to NavAbility
 
-query = gql('''
+query = gql(
+    """
     query  {
         VARIABLE(filter: {
             session: {
-            #   id: \$sessionId, 
+            #   id: \$sessionId,
               robot: {
-                # id: \$robotId, 
+                # id: \$robotId,
                 user: {
                   id: "Guest"
                 }}}}) {
@@ -66,18 +82,21 @@ query = gql('''
             }
         }
     }
-''')
+"""
+)
 
 
 ### Push to RVIZ
 
 result = client.execute(query)
-len(result['VARIABLE'])
-## 
-query = gql('''
+len(result["VARIABLE"])
+##
+query = gql(
+    """
     subscription solverUpdates {
         ...
     }
-''')
+"""
+)
 for result in client.subscribe(query):
-    print (result)
+    print(result)

2d45007fe65d9e41f792875e287c0e415e0552ee
Sam Claassens <sam@globus.org>
2021-12-27 22:56:05 -0600

    Updating status messages

--- src/navability/entities/Client.py
+++ src/navability/entities/Client.py
@@ -1,7 +1,26 @@
 from dataclasses import dataclass
-from typing import ClassVar
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
+
+
+@dataclass()
+class Client:
+    userId: str
+    robotId: str
+    sessionId: str
+
+    def __repr__(self):
+        return f"<Client(userId={self.userId}, robotId={self.robotId}, sessionId={self.sessionId})>"  # noqa: E501, B950
+
+    def dump(self):
+        return ClientSchema().dump(self)
+
+    def dumps(self):
+        return ClientSchema().dumps(self)
+
+    @staticmethod
+    def load(data):
+        return ClientSchema().load(data)
 
 
 class ClientSchema(Schema):
@@ -12,19 +31,6 @@ class ClientSchema(Schema):
     class Meta:
         ordered = True
 
-
-@dataclass()
-class Client:
-    schema: ClassVar[ClientSchema] = ClientSchema()
-    userId: str
-    robotId: str
-    sessionId: str
-
-    def __repr__(self):
-        return f"<Client(userId={self.userId}, robotId={self.robotId}, sessionId={self.sessionId})>"  # noqa: E501, B950
-
-    def dump(self):
-        return Client.schema.dump(self)
-
-    def dumps(self):
-        return Client.schema.dumps(self)
+    @post_load
+    def marshal(self, data, **kwargs):
+        return Client(**data)
--- src/navability/entities/StatusMessage.py
+++ src/navability/entities/StatusMessage.py
@@ -1,8 +1,31 @@
+from dataclasses import dataclass
 from datetime import datetime
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
 
 from navability.entities.Client import Client, ClientSchema
+
+
+@dataclass()
+class StatusMessage:
+    requestId: str
+    action: str
+    state: str
+    timestamp: str
+    client: Client
+
+    def __repr__(self):
+        return f"<StatusMessage(requestId={self.requestId}, timestamp={self.timestamp}, client={self.client}, action={self.action}, state={self.state})>"
+
+    def dump(self):
+        return StatusMessage.schema.dump(self)
+
+    def dumps(self):
+        return StatusMessage.schema.dumps(self)
+
+    @staticmethod
+    def load(data):
+        return StatusMessageSchema().load(data)
 
 
 class StatusMessageSchema(Schema):
@@ -12,29 +35,9 @@ class StatusMessageSchema(Schema):
     timestamp = fields.DateTime(required=True)
     client = fields.Nested(ClientSchema, required=True)
 
+    class Meta:
+        ordered = True
 
-class StatusMessage:
-    schema: StatusMessageSchema = StatusMessageSchema()
-
-    def __init__(
-        self,
-        requestId: str,
-        action: str,
-        state: str,
-        timestamp: datetime,
-        client: Client,
-    ):
-        self.requestId = requestId
-        self.action = action
-        self.state = state
-        self.timestamp = timestamp
-        self.client = client
-
-    def __repr__(self):
-        return f"<StatusMessage(requestId={self.requestId})>"
-
-    def dump(self):
-        return StatusMessage.schema.dump(self)
-
-    def dumps(self):
-        return StatusMessage.schema.dumps(self)
+    @post_load
+    def marshal(self, data, **kwargs):
+        return StatusMessage(**data)

982497efe758c1fed76586aa5b234a8bad8c3b30
Sam Claassens <sam@globus.org>
2022-01-17 09:09:52 -0600

    Adding release information

--- setup.py
+++ setup.py
@@ -14,12 +14,14 @@ if sys.version_info < (3, 0):
 setup(
     name="navabilitysdk",
     version="0.0.1",
+    license="Apache",
     author="NavAbility",
     package_dir={"": "src"},
     include_package_data=True,
     packages=find_packages("src", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    entry_points={"console_scripts": ["navability = navability.main:cli"]},
+    # entry_points={"console_scripts": ["navability = navability.main:cli"]},
     python_requires=">=3.5",
+    download_url="https://github.com/NavAbility/NavAbilitySDK.py/archive/refs/tags/v0.0.1.tar.gz",
     long_description="NavAbility SDK",
     install_requires=[
         "click>=8.0.0,<9",

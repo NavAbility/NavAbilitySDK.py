1d6414a7da3032206d0d70740f4bc8c0c8b370d1
Johannes Terblanche <6612981+Affie@users.noreply.github.com>
2023-05-10 12:33:25 +0200

    Fix blobEntries and getBlob (#134)

--- src/navability/services/blobstore.py
+++ src/navability/services/blobstore.py
@@ -1,9 +1,7 @@
 import logging
-import warnings
 import requests
-from typing import List
-
-from gql import gql
+import asyncio
+# from gql import gql
 
 from dataclasses import dataclass
 
@@ -17,12 +15,6 @@ from navability.entities.navabilityclient import (
     NavAbilityClient,
     QueryOptions,
 )
-# from navability.entities.querydetail import QueryDetail
-from navability.entities.blob.blobentry import (
-    BlobEntry,
-    BlobEntrySchema,
-)
-
 
 logger = logging.getLogger(__name__)
 
@@ -69,46 +61,12 @@ async def createDownload(
     )
 
     # TODO error checking
-    if not 'createDownload' in res:
+    if 'createDownload' not in res:
         raise ValueError('Cannot create download for ', userLabel, " seeking ", blobId)
     return res['createDownload']
 
 
-async def createUpload(
-    client: NavAbilityClient,
-    blobLabel: str,
-    blobSize: int,
-    parts: int = 1,
-):
-    """ Request URLs for data blob upload.
-
-    Args:
-        client (NavAbilityClient): The NavAbility client.
-        blobLabel (String): human readable blob label (aka filename).
-        filesize (Int): total number of bytes to upload. 
-        parts (Int): Split upload into multiple blob parts, 
-        FIXME currently only supports parts=1.
-
-    Returns:
-        str: The dedicated upload URL
-    """
-    params = {
-        "blobLabel": blobLabel,
-        "blobSize": blobSize,
-        "parts": parts
-    }
-    logger.debug(f"Query params: {params}")
-    res = await client.mutate(
-        MutationOptions(
-            gql(GQL_CREATEUPLOAD),
-            params,
-        )
-    )
-    # TODO error handling
-    return res['createUpload']
-
-
-async def getBlob(
+async def getBlobAsync(
     store: NavAbilityBlobStore,
     blobId: str,
 ):
@@ -126,69 +84,112 @@ async def getBlob(
     return resp.content
 
 
-async def completeUploadSingle(
-    client: NavAbilityClient,
+def getBlob(
+    store: NavAbilityBlobStore,
     blobId: str,
-    uploadId: str,
-    eTag: str,
 ):
-    #
-    params = {
-        "blobId": blobId,
-        "uploadId": uploadId,
-        "eTag": eTag
-    }
-    logger.debug(f"Query params: {params}")
-    res = await client.mutate(
-        MutationOptions(
-            gql(GQL_COMPLETEUPLOAD_SINGLE),
-            params,
-        )
-    )
-    # TODO error handling
-    return res['completeUpload']
+    tsk = getBlobAsync(store, blobId)
+    return asyncio.run(tsk)
 
 
-async def addBlob(
-    client: Client,
-    blobLabel: str,
-    blob
-):
-    """Push a data blob to user and get a unique identifier back.
+#TODO 
+# async def createUpload(
+#     client: NavAbilityClient,
+#     blobLabel: str,
+#     blobSize: int,
+#     parts: int = 1,
+# ):
+#     """ Request URLs for data blob upload.
 
-    Args:
-        client (NavAbilityClient): The NavAbility client for handling requests.
-        blobLabel (str): Human readable label for the blob (aka filename).
-        blob (bytes): Actual data blob as a chunk of data.
+#     Args:
+#         client (NavAbilityClient): The NavAbility client.
+#         blobLabel (String): human readable blob label (aka filename).
+#         filesize (Int): total number of bytes to upload. 
+#         parts (Int): Split upload into multiple blob parts, 
+#         FIXME currently only supports parts=1.
 
-    Returns:
-        blobId (String): The unique blob identifier of the data.
-    """
-    blobSize = len(blob)
-    upd = await createUpload(client, blobLabel, blobSize)
-    url = upd['parts'][0]['url']
-    uploadId = upd['uploadId']
-    blobId = upd['file']['id']
+#     Returns:
+#         str: The dedicated upload URL
+#     """
+#     params = {
+#         "blobLabel": blobLabel,
+#         "blobSize": blobSize,
+#         "parts": parts
+#     }
+#     logger.debug(f"Query params: {params}")
+#     res = await client.mutate(
+#         MutationOptions(
+#             gql(GQL_CREATEUPLOAD),
+#             params,
+#         )
+#     )
+#     # TODO error handling
+#     return res['createUpload']
+
+
+# async def completeUploadSingle(
+#     client: NavAbilityClient,
+#     blobId: str,
+#     uploadId: str,
+#     eTag: str,
+# ):
+#     #
+#     params = {
+#         "blobId": blobId,
+#         "uploadId": uploadId,
+#         "eTag": eTag
+#     }
+#     logger.debug(f"Query params: {params}")
+#     res = await client.mutate(
+#         MutationOptions(
+#             gql(GQL_COMPLETEUPLOAD_SINGLE),
+#             params,
+#         )
+#     )
+#     # TODO error handling
+#     return res['completeUpload']
+
+
+# async def addBlob(
+#     client: Client,
+#     blobLabel: str,
+#     blob
+# ):
+#     """Push a data blob to user and get a unique identifier back.
+
+#     Args:
+#         client (NavAbilityClient): The NavAbility client for handling requests.
+#         blobLabel (str): Human readable label for the blob (aka filename).
+#         blob (bytes): Actual data blob as a chunk of data.
+
+#     Returns:
+#         blobId (String): The unique blob identifier of the data.
+#     """
+#     blobSize = len(blob)
+#     upd = await createUpload(client, blobLabel, blobSize)
+#     url = upd['parts'][0]['url']
+#     uploadId = upd['uploadId']
+#     blobId = upd['file']['id']
     
-    headers = {
-        'Content-Length': str(blobSize),
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'cross-site',
-        'Sec-GPC': '1',
-        'Connection': 'keep-alive'
-    }
+#     headers = {
+#         'Content-Length': str(blobSize),
+#         'Accept': 'application/json, text/plain, */*',
+#         'Accept-Encoding': 'gzip, deflate, br',
+#         'Sec-Fetch-Dest': 'empty',
+#         'Sec-Fetch-Mode': 'cors',
+#         'Sec-Fetch-Site': 'cross-site',
+#         'Sec-GPC': '1',
+#         'Connection': 'keep-alive'
+#     }
 
-    # do the upload
-    r = requests.put(url, data=blob, headers=headers)
+#     # do the upload
+#     r = requests.put(url, data=blob, headers=headers)
     
-    # Extract eTag
-    eTag = r.headers['eTag']
+#     # Extract eTag
+#     eTag = r.headers['eTag']
 
-    # close out the upload
-    res = await completeUploadSingle(client, blobId, uploadId, eTag)
+#     # close out the upload
+#     res = await completeUploadSingle(client, blobId, uploadId, eTag)
 
-    return blobId
+#     return blobId
 
